ecc7c8d450576e9d4c443f1f80fc8f4f2d35a083
Albert-ca <albertohuamayalli@gmail.com>
2025-10-16 10:43:43 -0500

    Implementare la clase VariableCuantitativa con sus métodos estadísticos

--- POO_LIBRERIA/variables_cuantitativas.py
+++ POO_LIBRERIA/variables_cuantitativas.py
@@ -1,1 +1,16 @@
-print("hola")
+import pandas as pd
+import numpy as np
+
+class Variable:
+    def __init__(self, nombre, datos):
+        self.nombre = nombre
+        self.datos = pd.Series(datos).dropna()  # elimina valores vacíos
+        self.tipo = self.detectar_tipo()
+        self.n = len(self.datos)
+
+    def detectar_tipo(self):
+        try:
+            self.datos.astype(float)
+            return "cuantitativa"
+        except ValueError:
+            return "cualitativa"
