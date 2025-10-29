e1886980ad4a23c16725b7d06382bb2dac9d51c1
Albert-ca <albertohuamayalli@gmail.com>
2025-10-28 22:20:29 -0500

    runner.py modificado

--- runner.py
+++ runner.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import math
 from POO_LIBRERIA.variables_cuantitativas import VariableCuantitativa
+from POO_LIBRERIA.variables_cuantitativas import VisualizadorEstadistico
 
 df = pd.read_csv('archivo_prueba.txt')
 datos_ingreso = df['Ingreso_Mensual']
@@ -13,3 +14,5 @@ statitics = VariableCuantitativa(datos_ingreso)
 print(f"La media es: {statitics.media():.2f}")
 print(f"La mediana es: {statitics.mediana():.2f}")
 print(f"La desviación estándar es: {statitics.desviacion_estandar():.2f}")
+
+print(f"Resumen Estadistico completo es:{statitics.resumen():.2f}")
