a811093e5d6b509c0e1cb5ae30de2380a54dfc37
Albert-ca <albertohuamayalli@gmail.com>
2025-10-29 22:46:19 -0500

    Probando las funciones de nuestra libreria en archivo runner.py usando el archivo_prueba.txt

--- runner.py
+++ runner.py
@@ -1,18 +1,59 @@
+
 import pandas as pd
 import math
-from POO_LIBRERIA.variables_cuantitativas import VariableCuantitativa
-from POO_LIBRERIA.variables_cuantitativas import VisualizadorEstadistico
+import matplotlib.pyplot as plt
+import POO_LIBRERIA.libreria_completa as mi_libreria
 
-df = pd.read_csv('archivo_prueba.txt')
-datos_ingreso = df['Ingreso_Mensual']
-print(datos_ingreso.head(6))
 
-statitics = VariableCuantitativa(datos_ingreso)
+mi_libreria.pd = pd
+mi_libreria.math = math
+mi_libreria.plt = plt
 
-# Probando algunas estadísticas creadas
+df = pd.read_csv("archivo_prueba.txt")
 
-print(f"La media es: {statitics.media():.2f}")
-print(f"La mediana es: {statitics.mediana():.2f}")
-print(f"La desviación estándar es: {statitics.desviacion_estandar():.2f}")
+try:
+    print("\n--- Probando VariableCuantitativa (Columna: Ingreso_Mensual) ---")
+    
+    datos_ingreso = df['Ingreso_Mensual']
 
-print(f"Resumen Estadistico completo es:{statitics.resumen():.2f}")
+    var_ingreso = mi_libreria.VariableCuantitativa(datos=datos_ingreso)
+    print(var_ingreso) 
+    media_calculada = var_ingreso.media()
+    mediana_calculada = var_ingreso.mediana()
+    
+    print(f"La Media es: {media_calculada:.2f}")
+    print(f"La Mediana es: {mediana_calculada}")
+
+    # Gráficos Cuantitativos 
+    visualizador_ingreso = mi_libreria.VisualizadorEstadistico(var_ingreso)
+    print("\nMostrando gráficos cuantitativos (Histograma y Caja)...")
+    visualizador_ingreso.graficar_histograma()
+    visualizador_ingreso.graficar_boxplot()
+
+except Exception as e:
+    print(f"Error en la prueba cuantitativa: {e}")
+
+
+
+try:
+    print("\n--- Probando VariableCualitativa (Columna: Preferencia) ---")
+    
+    datos_preferencia = df['Preferencia']
+    
+    var_preferencia = mi_libreria.VariableCualitativa(datos_preferencia, "Preferencia")
+
+    
+    var_preferencia.resumen()
+    
+    var_preferencia.porcentaje_categoria("Tecnología")
+
+    #  Gráficos Cualitativos 
+    visualizador_preferencia = mi_libreria.VisualizadorCualitativo(var_preferencia)
+    print("\nMostrando gráficos cualitativos (Pastel y Barras)...")
+    visualizador_preferencia.graficar_pastel()
+    visualizador_preferencia.graficar_barras()
+
+except Exception as e:
+    print(f"Error en la prueba cualitativa: {e}")
+
+
